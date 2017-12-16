308011cc2d68168191beffd46c45af331a334ec2
admin <admin@admin-PC>
2017-12-16 12:28:23 +0530

    sample project push to git

---
+++ Documents/workspace-sts-3.9.1.RELEASE/sample/src/dev/welcome.java
@@ -0,0 +1,11 @@
+package dev;
+
+public class welcome {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to devopes");
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Documents/workspace-sts-3.9.1.RELEASE/sample/src/test/t1.java
@@ -0,0 +1,11 @@
+package test;
+
+public class t1 {
+
+	public static void main(String[] args) {
+		System.out.println("hi");
+		// TODO Auto-generated method stub
+
+	}
+
+}

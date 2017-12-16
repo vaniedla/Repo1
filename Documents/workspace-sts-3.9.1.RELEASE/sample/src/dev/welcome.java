94540f7887a9d7aa19983c94a077e997f848f300
admin <admin@admin-PC>
2017-12-16 12:34:26 +0530

    fhgfjf

--- Documents/workspace-sts-3.9.1.RELEASE/sample/src/dev/welcome.java
+++ Documents/workspace-sts-3.9.1.RELEASE/sample/src/dev/welcome.java
@@ -6,7 +6,7 @@ public class welcome {
 		System.out.println("welcome to devopes");
 		System.out.println("welcome");
 		// TODO Auto-generated method stub
-
+		System.out.println("welcome to devopes");
 	}
 
 }

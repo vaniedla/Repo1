8d3d59cdb7bcfb1a28607ff689155c4afa5ac733
vaniedla <edla.vani@gmail.com>
2018-01-06 11:19:01 +0530

    commiting

---
+++ Documents/workspace-sts-3.9.1.RELEASE/tesingmaven/src/main/java/com/t2/tesingmaven/App.java
@@ -0,0 +1,13 @@
+package com.t2.tesingmaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
